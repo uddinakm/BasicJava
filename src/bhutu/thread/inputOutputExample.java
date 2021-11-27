6bd0ed41af23c31dde6a50ba839e336c504f0fb8
kamalUddin <uddinakm@yahoo.com>
2021-11-27 11:57:56 -0800

    Added variable and input output example files

---
+++ src/bhutu/thread/inputOutputExample.java
@@ -0,0 +1,15 @@
+package bhutu.thread;
+
+public class inputOutputExample {
+
+	public static void main(String[] args) {
+		// Input output examples console out put
+		
+		System.out.println("Enter Something: ");
+		
+		String x=System.console().readLine();
+		
+		System.out.println("You have entered: "+x);
+	}
+
+}
---
+++ src/bhutu/thread/variableExamples.java
@@ -0,0 +1,43 @@
+package bhutu.thread;
+
+public class variableExamples {
+
+	static final int noOfDays=365;
+	
+	static final float pi=3.14f;
+	
+	
+	public static void main(String[] args) {
+		//java variables examples
+		
+		short x=10, y=5, z=1;
+		byte B=17;
+		long w=21345600000L;
+		double d=5.01;
+		float f=5.01f;
+		char ch='a';
+		String b="Hello World";
+		boolean isDone=false;
+		
+		
+		
+		
+		
+		System.out.println("Short data: "+x+" " + y + " "+z);	
+		
+		System.out.println("Byte : "+B);
+		
+		System.out.println("Long: "+w);
+		System.out.println("Double: "+d);
+		System.out.println("Float: "+f);
+		System.out.println("Char: "+ch);
+		System.out.println("String: "+b);
+		System.out.println("Bool: "+isDone);
+		
+		System.out.println(noOfDays);
+		System.out.println(pi);
+		
+		
+	}
+
+}
